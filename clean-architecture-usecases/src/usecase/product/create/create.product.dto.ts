9f81e9911c8a4f71542a885493a2cca66887ee12
Rhayana <rhayana@MacBook-Air-de-Rhayana.local>
2025-06-08 09:18:56 -0300

    first commit

---
+++ clean-architecture-usecases/src/usecase/product/create/create.product.dto.ts
@@ -0,0 +1,10 @@
+export interface inputCreateProductDto {
+    name: string;
+    price: number;
+}
+
+export interface OutputCreateProductDto {
+    id: string;
+    name: string;
+    price: number;
+}
